27ba6f5004694fbb8255e7a0aec6d0c031090f2d
Alisson Vieira <133975912+alissonvmatias@users.noreply.github.com>
2025-04-12 10:05:26 -0300

    Criando as rotas login e logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MasterController;
 use Illuminate\Support\Facades\Route;
 
+//auth routes
+route::get('/login', [AuthController::class, 'login']);
+route::get('/logout', [AuthController::class, 'logout']);
