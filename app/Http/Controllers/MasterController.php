2e607a34e67e7d7a3dbdd1260c5603e3c899ac77
Alisson Vieira <133975912+alissonvmatias@users.noreply.github.com>
2025-04-12 02:07:23 -0300

    testando ter um laytou padrao e adicionando ele em outras views

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -8,8 +8,17 @@ class MasterController extends Controller
 {
     public function index($value)
     {
-
-    return view('master', ['value' => $value]);
-
+        return view('master', ['value' => $value]);
     }
+
+    public function page2($value)
+    {
+        return view('page2', ['value' => $value]);
+    }
+
+    public function page3($value)
+    {
+        return view('page3', ['value' => $value]);
+    }
+
 }
---
+++ resources/views/page3.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome view and Blade</h1>
+    <h3>page3</h3>
+    <h3>the value is: {{ $value }}</h3>
+</body>
+</html>
