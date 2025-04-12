baa6a5be561eb7ce3875c72f885926e0bb1a5241
Alisson Vieira <133975912+alissonvmatias@users.noreply.github.com>
2025-04-12 01:17:14 -0300

    Alterando routes > web , adicionando rota /about e print hello word na /

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    print('Hello World');
+});
+
+Route::get('/about', function ( ){
+    print ('About US');
 });
