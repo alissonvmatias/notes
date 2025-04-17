ece8baa4c6f7b004f3b62c734da894f2d22d80ce
Alisson Vieira <133975912+alissonvmatias@users.noreply.github.com>
2025-04-16 23:10:20 -0300

    Incluindo validação para text_username e text_password.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,10 +15,21 @@ class AuthController extends Controller
     {
         //form validation
         $request->validate(
+            //rules
             [
-                'text_username' => 'required' , //caso houvesse mais alguma validação poderia colocar o | e ir adicionando as outiras  ( required | asjkdlasdj | dhsajdhkjashd)
+                'text_username' => 'required|email' , //caso houvesse mais alguma validação poderia colocar o | e ir adicionando as outiras  ( required | asjkdlasdj | dhsajdhkjashd)
                 // outra forma seria por array ['required', 'dasdasd', 'dashjdhaks']
-                'text_password' => 'required'
+                'text_password' => 'required|min:6|max:16'
+            ], 
+            
+            // error messages
+            [
+                'text_username.required' => 'O usernamen é obrigatório',
+                'text_username.email' => 'O usuário deve ser um e-mail',
+                'text_password.required' => 'A senha deve ser obrigatória',
+                'text_password.min' => 'A senha deve conter no mínimo :min caracteres',
+                'text_password.max' => 'A senha deve conter no máximo :max caracteres',
+
             ]
 
             );
