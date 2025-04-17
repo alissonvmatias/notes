6b679fc44dfae4056ac2f6e11389834e3d1743f5
Alisson Vieira <133975912+alissonvmatias@users.noreply.github.com>
2025-04-16 22:41:42 -0300

    adicionando retorno ao clicar em login submit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,15 @@ class AuthController extends Controller
         return view ('login');
     }
 
-    public function logout() {
+    public function loginSubmit(Request $request) 
+    {
+        echo $request->input('text_username'); // serve para retornar o valor ( usado para verificar se esta trazendo o valor correto)
+        echo '<br>';
+        echo $request->input('text_password'); // serve para retornar o valor ( usado para verificar se esta trazendo o valor correto)
+    }
+
+    public function logout() 
+    {
         print "logout";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 //auth routes
 route::get('/login', [AuthController::class, 'login']);
+route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 route::get('/logout', [AuthController::class, 'logout']);
