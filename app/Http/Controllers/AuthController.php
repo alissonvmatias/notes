1303861cb0dc8bb346a83dd59dcb769bc6cbff25
Alisson Vieira <133975912+alissonvmatias@users.noreply.github.com>
2025-04-30 22:43:22 -0300

    adicionando base de dados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use PDOException;
 
 class AuthController extends Controller
 {
@@ -38,7 +40,19 @@ class AuthController extends Controller
             $username = $request->input('text_username');
             $password = $request->input('text_password');
 
-            echo "OK!"; // esse OK é para caso passe as validaçoes ele retorne esse OK
+            //echo "OK!"; // esse OK é para caso passe as validaçoes ele retorne esse OK
+
+            //teste database connection
+            try{
+                DB::connection()->getPdo();
+                echo "Connection is OK!";
+            }
+            
+            catch(PDOException $e) {
+                echo "Connection failed" . $e->getMessage();
+            }
+
+            echo "Fim!";
     }
 
     public function logout() 
